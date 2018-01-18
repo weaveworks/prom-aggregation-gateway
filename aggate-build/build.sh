1b5f2963d74999d673b8a7ba928575d6e39d5cdc
Bryan Boreham <bjboreham@gmail.com>
2018-01-18 17:16:15 +0000

    Fix lint issues

--- aggate-build/build.sh
+++ aggate-build/build.sh
@@ -8,8 +8,8 @@ SRC_PATH=$GOPATH/src/github.com/weaveworks/prom-aggregation-gateway
 # will have awkward ownership.  So we switch to a user with the
 # same user and group IDs as source directory.  We have to set a
 # few things up so that sudo works without complaining later on.
-uid=$(stat --format="%u" $SRC_PATH)
-gid=$(stat --format="%g" $SRC_PATH)
+uid=$(stat --format="%u" "$SRC_PATH")
+gid=$(stat --format="%g" "$SRC_PATH")
 echo "weave:x:$uid:$gid::$SRC_PATH:/bin/sh" >>/etc/passwd
 echo "weave:*:::::::" >>/etc/shadow
 echo "weave	ALL=(ALL)	NOPASSWD: ALL" >>/etc/sudoers
